db2458f1c48a0f07ba18958b150bb36c8c94d9e9
freddyberg96 <freddyberg96@web.de>
2019-02-07 18:34:15 +0100

    Version PPM V2: -added print out function -added button to print out

--- app/src/main/java/edmt/dev/androidcamera2api/DisplayMessageActivity.java
+++ app/src/main/java/edmt/dev/androidcamera2api/DisplayMessageActivity.java
@@ -2,14 +2,25 @@
 package edmt.dev.androidcamera2api;
 
 import android.content.Context;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private List<String> savedDates;
+    private List<String> savedMessages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +28,29 @@ public class DisplayMessageActivity extends AppCompatActivity {
         //Start window
         setContentView(R.layout.activity_display_message);
 
+        //Get Button object
+        Button buttonPrint = (Button) findViewById(R.id.buttonPrint);
+
         //Get the message data
-        List<String> savedDates = StorageManager.getInstance().returnListDates();
-        List<String> savedMessages = StorageManager.getInstance().returnListMessages();
+        savedDates = StorageManager.getInstance().returnListDates();
+        savedMessages = StorageManager.getInstance().returnListMessages();
 
 
         // Get TableLayout object in layout xml
-        final TableLayout tableLayout = (TableLayout)findViewById(R.id.table_layout_table);
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.table_layout_table);
 
+        //Get context
         Context context = getApplicationContext();
 
 
-        for (int i=0; i<savedMessages.size() && i<savedDates.size(); i++) {
+        for (int i = 0; i < savedMessages.size() && i < savedDates.size(); i++) {
 
             // Create a new table row.
             TableRow tableRow = new TableRow(context);
 
             // Set new table row layout parameters.
             TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(10,10,10,10);
+            layoutParams.setMargins(10, 10, 10, 10);
             tableRow.setLayoutParams(layoutParams);
 
             // Add a TextView in the first column.
@@ -48,10 +63,47 @@ public class DisplayMessageActivity extends AppCompatActivity {
             textView2.setText(savedMessages.get(i));
             tableRow.addView(textView2, 1);
 
+            //Add the view
             tableLayout.addView(tableRow);
 
         }
 
+        //Set up listener and handler of button click
+        buttonPrint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //set up the file path
+                File file01 = new File(Environment.getExternalStorageDirectory() + "/"+savedDates.get(savedDates.size()-1)+".txt");
+                //Stream of text file
+                FileWriter fileWriter01 = null;
+                try {
+                    fileWriter01 = new FileWriter(file01);
+
+                for(int i=0; i<savedDates.size() && i<savedMessages.size(); i++) {
+                    fileWriter01.write(savedDates.get(i)+","+savedMessages.get(i)+"\n");
+                }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                } finally {
+                    try {
+                        if (fileWriter01 != null) {
+                            fileWriter01.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(),"Data saved as .txt file on internal storage!",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
 
     }
+
 }
